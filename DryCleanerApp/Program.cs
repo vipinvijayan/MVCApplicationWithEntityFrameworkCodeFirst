007dc02deef6bad50c4ce21ea3ae3ac67ba649a7
Vipin <vipin@q5soft.com>
2023-11-08 15:44:49 +0530

    Initial Commit
    Mvc Application With
    Create Company
    List Company
    Delete COmpany
    Edit Company

---
+++ DryCleanerApp/Program.cs
@@ -0,0 +1,47 @@
+
+using DryCleanerAppBussinessLogic;
+using DryCleanerAppBussinessLogic.Implementation;
+using DryCleanerAppBussinessLogic.Interfaces;
+using DryCleanerAppDataAccess.IRepository;
+using DryCleanerAppDataAccess.Repository;
+using DryCleanerAppDataAccess.DataAccess;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+string? dbConnectionString = builder.Configuration.GetConnectionString("AppConnection");
+
+builder.Services.AddDbContext<DryCleanerContext>(options =>
+options.UseMySql(dbConnectionString, ServerVersion.AutoDetect(dbConnectionString))
+);
+builder.Services.AddRepositoryDependecies();
+builder.Services.AddScoped<ICompanyB, CompanyB>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ DryCleanerAppDataAccess/DataAccess/ApplicationDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DryCleanerAppDataAccess.DataAccess
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<DryCleanerContext>
+    {
+        public DryCleanerContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<DryCleanerContext>();
+            optionsBuilder.UseMySql("Your Connectionstring here"));
+
+            return new DryCleanerContext(optionsBuilder.Options);
+        }
+    }
+}
